748d4cd1134a3d5293bf75ade859cea26bfbe3a2
myertaye <medetyertayev@gmail.com>
2024-08-26 01:28:44 +0500

    Finished the task '#3. Information'

--- features/infoErrorHandlers.go
+++ features/infoErrorHandlers.go
@@ -7,7 +7,7 @@ import (
 
 func checkBrandAndIssuerInput(brand, issuer string) {
 	if brand == "" || issuer == "" {
-		fmt.Fprintln(os.Stderr, "Please enter brand or/and issuer name")
+		fmt.Fprintln(os.Stderr, "Please enter file name/s")
 		os.Exit(1)
 	} else if brand != "brands.txt" || issuer != "issuers.txt" {
 		fmt.Fprintln(os.Stderr, "Incorrect file name/s; must be 'brands.txt' and 'issuers.txt'")
@@ -18,6 +18,13 @@ func checkBrandAndIssuerInput(brand, issuer string) {
 func checkArgsLength(cardNumbers []string) {
 	if len(cardNumbers) == 0 {
 		fmt.Fprintln(os.Stderr, "There are no args, enter a card number")
+		os.Exit(1)
+	}
+}
+
+func check(e error) {
+	if e != nil {
+		fmt.Fprintln(os.Stderr, e, ": Incorrect path")
 		os.Exit(1)
 	}
 }
--- features/information.go
+++ features/information.go
@@ -3,31 +3,94 @@ package features
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
-func Information(stdin bool, brand, issuer string, cardNumbers []string) {
+func Information(stdin bool, brandFileName, issuerFileName string, cardNumbers []string) {
 	fmt.Println(" stdin:", stdin)
-	fmt.Println(" brand:", brand)
-	fmt.Println(" issuer", issuer)
+	fmt.Println(" brand:", brandFileName)
+	fmt.Println(" issuer", issuerFileName)
 	fmt.Println(" tail:", cardNumbers)
+	fmt.Println()
 
-	checkBrandAndIssuerInput(brand, issuer)
+	checkBrandAndIssuerInput(brandFileName, issuerFileName)
 	checkArgsLength(cardNumbers)
 
 	for _, cardNumber := range cardNumbers {
 		if checkCardValidity(cardNumber) && (len(cardNumber) >= 13 && len(cardNumber) <= 16) && (luhnsAlgorithm(strToNum(cardNumber))) {
-			// cardBrand
-			// CardIssuer
+			// read from files in a given dir; check for nil pointer
+			fileContentBrands, err1 := os.ReadFile("./text/" + brandFileName)
+			check(err1)
+			fileContentIssuers, err2 := os.ReadFile("./text/" + issuerFileName)
+			check(err2)
+
+			// conversion of byte file contents to string type; dividing the big string into smaller ones and storing in a slice
+			cardBrands := strings.Split(string(fileContentBrands), "\n")
+			cardIssuers := strings.Split(string(fileContentIssuers), "\n")
+			// fmt.Println(cardBrands)
+			// fmt.Println(cardIssuers)
+
+			brand := findCardBrand(cardBrands, cardNumber)
+			issuer := findCardIssuer(cardIssuers, cardNumber)
 
 			fmt.Fprintln(os.Stdout, cardNumber)
 			fmt.Fprintln(os.Stdout, "Correct: yes")
-			fmt.Fprintln(os.Stdout, "Card Brand:")
-			fmt.Fprintln(os.Stdout, "Card Issuer:")
+			fmt.Fprintln(os.Stdout, "Card Brand:", brand)
+			fmt.Fprintln(os.Stdout, "Card Issuer:", issuer)
 		} else {
 			fmt.Fprintln(os.Stderr, cardNumber)
 			fmt.Fprintln(os.Stderr, "Correct: no")
 			fmt.Fprintln(os.Stderr, "Card Brand: -")
 			fmt.Fprintln(os.Stderr, "Card Issuer: -")
+			fmt.Println()
 		}
 	}
+}
+
+func findCardBrand(cardBrands []string, cardNumber string) string {
+	for _, v := range cardBrands {
+		if strings.Count(v, ":") > 1 || strings.Count(v, ":") == 0 {
+			fmt.Fprintln(os.Stderr, "Incorrect format - too many : (two-dots) symbols or zero separators; want only 1")
+			fmt.Println("cardBrands")
+			os.Exit(1)
+		}
+
+		splittedCardBrand := strings.Split(v, ":")
+		if splittedCardBrand[0] == "" || splittedCardBrand[1] == "" {
+			fmt.Fprintln(os.Stderr, "Incorrect input format of a card type: empty name or number prefix")
+			os.Exit(1)
+		}
+
+		brandName := splittedCardBrand[0]
+		startingDigits := splittedCardBrand[1]
+
+		if startingDigits == cardNumber[:len(startingDigits)] {
+			return brandName
+		}
+	}
+	return "-"
+}
+
+func findCardIssuer(cardIssuers []string, cardNumber string) string {
+	for _, v := range cardIssuers {
+		if strings.Count(v, ":") != 1 {
+			fmt.Fprintln(os.Stderr, "Incorrect format - want only 1 colon in each line")
+			os.Exit(1)
+		}
+
+		splittedCardIssuer := strings.Split(v, ":")
+		if splittedCardIssuer[0] == "" || splittedCardIssuer[1] == "" {
+			fmt.Fprintln(os.Stderr, "Incorrect input format of a card type: empty name and/or number prefix")
+			os.Exit(1)
+		}
+
+		issuerName := splittedCardIssuer[0]
+		startingDigits := splittedCardIssuer[1]
+
+		if startingDigits == cardNumber[:len(startingDigits)] {
+			return issuerName
+		}
+
+	}
+	return "-"
 }
